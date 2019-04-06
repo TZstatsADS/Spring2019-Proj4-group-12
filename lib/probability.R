f02e47d31b5d0adcad75bd7414d584a583c0679b
Zeyu Yang <zy2327@columbia.edu>
2019-04-06 14:25:58 -0400

    update

---
+++ lib/probability.R
@@ -0,0 +1,15 @@
+source("../output/ground_truth_frequency.RData")
+
+probability <- function()
+{
+  # parameter explaination
+  ## index of error word in 
+  
+  
+  # without context
+  
+  
+  # with context
+  
+  
+}
