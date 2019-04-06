5f0cbf05f4b50ed737d71174529d652d45de692a
Zeyu Yang <zy2327@columbia.edu>
2019-04-06 15:05:49 -0400

    update

---
+++ lib/potential correction.R
@@ -0,0 +1,8 @@
+potential_correction <- function(typo)
+{
+  # Input typo, output a list of potential correction
+  
+  
+}
+
+
--- lib/probability.R
+++ lib/probability.R
@@ -1,15 +1,29 @@
 source("../output/ground_truth_frequency.RData")
 
-probability <- function()
+probability <- function(i,j,typo,correction)
 {
   # parameter explaination
-  ## index of error word in 
-  
+  ## index of error word in tesseract_vec: i_th row, j_th column
+  ## typo: the wrong word; correction: the potential correction word
   
   # without context
+  N <- sum(ground_truth_freq[,2])
+  V <- nrow(ground_truth_freq)
+  freq <- ifelse((correction %in% ground_truth_freq$tokens),
+                 ground_truth_freq[ground_truth_freq$tokens==correction,2],
+                 0)
+  Pr_c <- (freq+0.5)/(N+V/2)
   
-  
+  Pr_t_c
   # with context
   
+  Pr_l_c
   
+  Pr_r_c
+  
+  # Combine probability
+  Pr = Pr_c*1*1*1
+  
+  return(Pr)
 }
+
